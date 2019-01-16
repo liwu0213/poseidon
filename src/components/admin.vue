0e0da5970c160a2593eca2c897c5670ce48068ca
muggle <1977339740@qq.com>
2019-01-16 22:56:16 +0800

    调整页面

---
+++ src/components/admin.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>管理员界面测试</h1>
+</template>
+
+<script>
+    export default {
+        name: "admin"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/components/login'
 import home from '../components/home'
-
+import admin from '@/components/admin'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +16,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/poseidon/oauth/admin',
+      name: 'admin',
+      component: admin
     }
   ]
 })

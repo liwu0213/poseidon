7553807ed9e26c2856ad5e1b46a6ddd5195fc904
muggle <yuanzening@hlram.cn>
2019-01-23 15:57:42 +0800

    dsdd

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app" style="margin-top: 0px;padding-top: 0px">
-      <div>{{userInfo.nickname}}</div>
-    <img v-bind:src="userInfo.imgUrl"/>
-    <router-view/>
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <div style="margin: auto;background-color: black ;width: 100px;display: inline-block" >ssss</div>
+          <i class="el-icon-setting" style="margin-right: 15px;"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+      </el-header>
+      <el-main style="padding: 0px">
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -17,7 +31,9 @@ export default {
         gender: null,
         email: null,
         imgUrl: null
-      }
+      },
+      menus: ['试验田', '博客', '资源共享', '盖楼', '关于本站'],
+      mini: ['登录', '注册', '消息']
     }
   },
   components: {},
@@ -49,5 +65,10 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
   }
 </style>
